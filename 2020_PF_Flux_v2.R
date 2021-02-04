23ff3d1fde2aa15039c850a49732f98b687b2f7a
Bryce Currey <57914237+Green-Currey@users.noreply.github.com>
2021-02-04 13:00:44 -0700

    Add files via upload

---
+++ 2020_PF_Flux_v2.R
@@ -0,0 +1,163 @@
+# Picaro flux analyses
+
+rm(list = ls())
+
+# Packages
+library(readr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+
+# Directory where PF monthly files are stored
+dir <- '~/Other Projects/Justin Picarro/'
+setwd(dir)
+
+# Data where flux data is accessed
+df <- read.csv('PF_Flux_2020.csv')
+
+# vector of months for finding and extracting the picarro data
+months <- c('January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December')
+
+# Dimensions of chamber ---------------------------------------------------
+
+d <- 0.2032 # diameter (m)
+r <- d/2 # radius (m)
+h <- 0.15 # height (m)
+
+A <- pi*r^2 # area (m^2)
+V <- A*h * 1000 # volume (liters)
+R <- .08314 # gas constant (LK mol -1) #updated from Christiansen
+
+
+
+
+# Flux calculation --------------------------------------------------------
+
+for (i in seq(nrow(df))) {
+  
+  # date of measurement (e.g. May 3rd, 2019)
+  date <- ymd(paste(df$Year[i],df$Month[i],df$Sample_Day[i]))
+  
+  # date in formate for extracting the data (eg 'May2019/03')
+  date.str <- paste0(months[month(date)],
+                     year(date), 
+                     '/', 
+                     str_pad(df$Sample_Day[i], width = 2, side = 'left', '0')
+  )
+  
+  # time on is when the button on the picarro was clicked to start recording of flux data
+  #     this is important for locating the flux.file in the the directory
+  time.on <- df$ON[i] 
+  minutes <- time.on %>% as.character() %>% substr(3,4) %>% as.numeric()
+
+  # For filtering the data, we use one minute past time on (time.on + 1)
+  #     and 5 minutes after that time.
+  #     This is complicated when the time goes over the hour (e.g. 10:57-11:02)
+  #     because 1057 + 5 = 1062 or 10:62, which is a fake time.
+  
+  # So, if time.on is 5 minutes from the next hour (e.g. 10:55-10:59), 
+  #     time.off (time.on+5) would be a nonexistent time (e.g., 10:60-10:64). 
+  #     To fix this, we add 100 to the nonexistent time.off to move up to the next hour (e.g., 10:62 -> 11:62)
+  #     then subtract off the excess 60 minutes to go back to the hour (e.g. 11:62 -> 11:02).
+  #     This is equivalent to adding 45 minutes to time.on (time.on + 5) + 100 - 60
+  # 
+  #     Thus, time.on = 10:57 now has a time.off that is 5 minutes later (11:02)
+  
+  if (minutes >= 55 & minutes < 59) {
+    
+    new.time.on <- time.on + 1
+    new.time.off <- (time.on + 45) + 1
+    
+  }
+  
+  # if time.on is :59 (e.g. 10:59), time.on+1 would make it a fake time (e.g., 10:60)
+  #     by adding 100, we move up to the next hour (e.g., 10:59 -> 11:59)
+  #     then subtract off the excess 59 minutes (11:59 -> 11:00).
+  #     This is the equivalent of adding 41 to time on (time.on + 100 - 59) 
+  # if time.on isn't :59, simply adds one (above).
+  if (minutes == 59) {
+    new.time.on <- time.on + 41
+    new.time.off <- new.time.on + 5
+    
+  }
+  
+  # Else this
+  
+  if (minutes < 55) {
+    new.time.on <- time.on + 1
+    new.time.off <- new.time.on + 5
+    
+  }
+  
+  # extracts the data file that corresponds with 'time.on' from the pathway created from 'dir' variable and 'date.str' variable
+  flux.file <- list.files(path = paste0(dir,date.str), pattern = paste(time.on), full.names = T)
+  
+  # checks if flux.file doesnt exist (i.e. there was no file corresponding to 'time.on' variable)
+  # also checks if flux.file is more than one file, indicating that the button was clicked twice in the same
+  # minute. This script chooses the second file created, corresponding to the second time the button was clicked.
+  if (flux.file %>% length == 0) { next }
+  if (flux.file %>% length > 1) { flux.file <- flux.file[2] }
+  
+  # formating the data.
+  # 1) reads the data from read_table
+  # 2) from the DATE and TIME columns from the picarro data file, creates one variable in mountain time (e.g. '2019-05-03 12:00:00 MDT')
+  # 3) puts the time variable into a numeric format (e.g. 12:34:51 turns into 123451)
+  # 4) chooses the variables of interest
+  # 5) filters the data between the time.on+1 and time.off+1 points.
+  
+  data <- read_table(flux.file) %>%
+    mutate(time = ymd_hms(paste(DATE, TIME), tz = 'America/Denver')) %>%
+    mutate(time = strftime(time, format = "%H%M%S") %>% as.numeric) %>%
+    select(time, N2O_dry, CO2, CH4_dry, NH3)%>% 
+    filter(time >= paste0(new.time.on,'00') %>% as.numeric) %>%
+    filter(time <= paste0(new.time.off,'00') %>% as.numeric) %>%
+    mutate(time2 = seq(n()))
+  
+  
+  
+  # obtaining the slope coefficints from linear models between time and flux
+  N2O.slope <- coefficients(lm(N2O_dry ~ time2, data))[2] %>% as.numeric
+  CO2.slope <- coefficients(lm(CO2 ~ time2, data))[2] %>% as.numeric
+  CH4.slope <- coefficients(lm(CH4_dry ~ time2, data))[2] %>% as.numeric
+  NH3.slope <- coefficients(lm(NH3 ~ time2, data))[2] %>% as.numeric
+  
+  
+  # flux calculations
+  N2O.flux = N2O.slope * (V / (A * R * (273.15 + df$Temp[i]))) * 3600 
+  CO2.flux = CO2.slope * (V / (A * R * (273.15 + df$Temp[i]))) # multiply my 0.001 to get mmol per hour
+  CH4.flux = CH4.slope * (V / (A * R * (273.15 + df$Temp[i]))) * 3600
+  NH3.flux = NH3.slope * (V / (A * R * (273.15 + df$Temp[i]))) * 3600
+  #####* 24 is to get to mmol per m^2 per day (not hour)
+  
+  # appending the data frame
+  df$Flux_N2O[i] <- N2O.flux
+  df$Mean_N2O[i] <- mean(data$N2O_dry, na.rm = T)
+  df$SD_N2O[i] <- sd(data$N2O_dry, na.rm = T)
+  
+  df$Flux_CO2[i] <- CO2.flux
+  df$Mean_CO2[i] <- mean(data$CO2, na.rm = T)
+  df$SD_CO2[i] <- sd(data$CO2, na.rm = T)
+  
+  df$Flux_CH4[i] <- CH4.flux
+  df$Mean_CH4[i] <- mean(data$CH4_dry, na.rm = T)
+  df$SD_CH4[i] <- sd(data$CH4_dry, na.rm = T)
+  
+  df$Flux_NH3[i] <- NH3.flux
+  df$Mean_NH3[i] <- mean(data$NH3, na.rm = T)
+  df$SD_NH3[i] <- sd(data$NH3, na.rm = T)
+}
+
+# rewrite data
+write.csv(df, '~/Dropbox/Bryce_Justin_Folder/Picarro/PF_Flux_Data_Picarro/PF_Flux_2020.csv')
---
+++ chamber_by_day_code_2021.R
@@ -0,0 +1,193 @@
+# Picaro flux analyses by single date + plots
+
+###General notes: Make sure that raw data file in dropbox has two digits. i.e the 3rd of May should be 03
+##make sure to clear global environment before every run
+
+rm(list = ls())
+
+# Packages
+library(readr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(ggpubr)
+
+# Directory where PF monthly files are stored
+dir <- 'c:/users/bryce/onedrive/Documents/Other Projects/Justin Picarro/'
+setwd(dir)
+
+
+# vector of months for finding and extracting the picarro data
+months <- c('January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December')
+
+# Dimensions of chamber ---------------------------------------------------
+
+d <- 0.2032 # diameter (m)
+r <- d/2 # radius (m)
+h <- 0.15 # height (m)
+
+A <- pi*r^2 # area (m^2)
+V <- A*h * 1000 # volume (liters)
+R <- .08314 # gas constant (LK mol-1) 
+
+
+
+# Plotting by sampling day and time ------------------------------------------------
+
+# Enter Year
+Year <- 2020
+df.main <- read_csv(paste0('PF_Flux_', Year, '.csv'))
+# searches for all days
+print(unique(df.main$Month))
+
+
+# Enter Month numerically
+Month <- 09
+m = months[Month]
+
+df <- df.main %>% filter(Month == m)# Filter by day
+Month <- str_pad(Month, width = 2, side = 'left', pad = '0')
+
+# searches for all days
+print(unique(df$Sample_Day))
+
+
+# Enter day
+Day <- 14
+# Filter by day
+df <- df %>% filter(Sample_Day == Day)
+Day <- str_pad(Day, width = 2, side = 'left', pad = '0')
+
+
+paste('Running script for:',m,Day,Year)
+
+for (i in seq(nrow(df))) {
+  
+  time.on <- df$ON[i]
+  minutes <- time.on %>% as.character() %>% substr(3,4) %>% as.numeric()
+  
+  # See main script (e.g., 2020_PF_Flux_v2.R) for more information on these if statements.
+  if (minutes >= 55 & minutes < 59) {
+    
+    new.time.on <- time.on + 1
+    new.time.off <- (time.on + 45) + 1
+    
+  }
+  
+  if (minutes == 59) {
+    new.time.on <- time.on + 41
+    new.time.off <- new.time.on + 5
+    
+  }
+  
+  if (minutes < 55) {
+    new.time.on <- time.on + 1
+    new.time.off <- new.time.on + 5
+    
+  }
+  
+  Tx <- df$Treatment[i]
+  Plot <- df$Plot[i]
+  # Finds specified file
+  
+  flux.file <- list.files(path = paste0(m,Year,'/',Day,'/'), pattern = paste0(Year, Month, Day, '-', time.on), full.names = T)
+  
+  # checks if flux.file doesnt exist (i.e. there was no file corresponding to 'time.on' variable)
+  # also checks if flux.file is more than one file, indicating that the button was clicked twice in the same
+  # minute. This script chooses the second file created, corresponding to the second time the button was clicked.
+  if (flux.file %>% length == 0) { next }
+  if (flux.file %>% length > 1) { flux.file <- flux.file[2] }
+  
+  # formating the data.
+  # 1) reads the data from read_table
+  # 2) from the DATE and TIME columns from the picarro data file, creates one variable in mountain time (e.g. '2019-05-03 12:00:00 MDT')
+  # 3) puts the time variable into a numeric format (e.g. 12:34:51 turns into 123451)
+  # 4) chooses the variables of interest
+  # 5) filters for times that occur after 'time.on' and before 10 minutes following 'time.on'
+  data <- read_table(flux.file) %>%
+    mutate(time = ymd_hms(paste(DATE, TIME), tz = 'America/Denver')) %>%
+    mutate(time = strftime(time, format = "%H%M%S") %>% as.numeric) %>%
+    mutate(time = time) %>%
+    dplyr::select(time, N2O_dry, CO2, CH4, NH3) %>%
+    filter(time >= paste0(new.time.on,'00') %>% as.numeric) %>%
+    filter(time <= paste0(new.time.off,'00') %>% as.numeric) %>%
+    mutate(time2 = seq(n()),
+           Treatment = rep(Tx),
+           Plot = rep(Plot),
+           Plot_Tx = paste0(rep(Plot),'. ', Tx),
+           On = rep(new.time.on))
+  
+  if (i == 1) {to.plot <- data} else {to.plot <- to.plot %>% rbind(data)}
+}
+
+cols <- c('grey47', 'darkolivegreen3', 'darkorange')
+
+N2O <- ggplot(data = to.plot) +
+  aes(x = time2, y = N2O_dry, group = Treatment, color = Treatment) +
+  geom_point() + geom_smooth() +
+  facet_grid(.~Plot_Tx, scales = 'free') +
+  scale_color_manual(values = cols) +
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = 'none') +
+  labs(x = '')
+
+
+
+CO2 <- ggplot(data = to.plot) +
+  aes(x = time2, y = CO2, group = Treatment, color = Treatment) +
+  geom_point() + geom_smooth() +
+  facet_grid(.~Plot_Tx, scales = 'free') +
+  scale_color_manual(values = cols) +
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = 'none') +
+  labs(x = '')
+
+
+
+CH3 <- ggplot(data = to.plot) +
+  aes(x = time2, y = CH4, group = Treatment, color = Treatment) +
+  geom_point() + geom_smooth() +
+  facet_grid(.~Plot_Tx, scales = 'free') +
+  scale_color_manual(values = cols) +
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = 'none') +
+  labs(x = '')
+
+
+
+NH3 <- ggplot(data = to.plot) +
+  aes(x = time2, y = NH3, group = Treatment, color = Treatment) +
+  geom_point() + geom_smooth() +
+  facet_grid(.~Plot_Tx, scales = 'free') +
+  scale_color_manual(values = cols) +
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = 'none') +
+  labs(x = '5 minute Interval')
+
+
+
+tiff(filename = paste0('Raw flux measurements for ',m,Day,'_',Year, '.tif'), height = 16, width = 16, res = 150, units = 'in')
+ggarrange(N2O, CO2, CH3, NH3,
+          nrow = 4, 
+          align = 'hv') %>%
+  annotate_figure(top = text_grob(paste0(m,' ',Day,', ',Year), size = 25))
+dev.off()
